11cdb54c49800a2fd9b2c4712b3095c6deba0880
Mallikarjun Hanagandi <mallikarjun.h@ladybirdweb.com>
2019-01-18 13:36:21 +0530

    v1.3
    * ChangeDetailsDataView - Part I

---
+++ Faveo Service Desk Pro/ChangeManagement/ChangeDetailData.h
@@ -0,0 +1,17 @@
+//
+//  ChangeDetailData.h
+//  Faveo Service Desk Pro
+//
+//  Created by Mallikarjun on 18/01/19.
+//  Copyright © 2019 Ladybird Web Solution Pvt Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ChangeDetailData : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
